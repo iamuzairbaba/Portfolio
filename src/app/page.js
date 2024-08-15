ae795493224da712ee1df1fdf4fad188a4a99aaf
iamuzairbaba <iamuziibaba@gmail.com>
2024-08-15 13:05:20 +0530

    Added skills and footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import Head from "next/head";
 import { BsMoonStarsFill } from "react-icons/bs";
 import {
@@ -19,21 +19,24 @@ import ogo from "../../public/ogo.png";
 import { useState } from "react";
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(true)
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div className={darkMode ? 'dark' : ""}>
+    <div className={darkMode ? "dark" : ""}>
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-800">
         <section className=" min-h-screen ">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-burtons dark:text-white">UZAIR BASHIR</h1>
+            <h1 className="text-xl font-burtons dark:text-white">
+              UZAIR BASHIR
+            </h1>
             <ul className="flex items-center">
               <li>
-                <BsMoonStarsFill className="cursor-pointer dark:text-white text-2xl" onClick={()=>setDarkMode(!darkMode)}/>
+                <BsMoonStarsFill
+                  className="cursor-pointer dark:text-white text-2xl"
+                  onClick={() => setDarkMode(!darkMode)}
+                />
               </li>
               <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                >
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8">
                   Resume
                 </a>
               </li>
@@ -43,7 +46,9 @@ export default function Home() {
             <h2 className="text-5xl py-2 text-teal-700 font-medium md:text-6xl">
               Uzair Bashir
             </h2>
-            <h3 className="text-2xl py-2 md:text-3xl dark:text-white">Front End Developer</h3>
+            <h3 className="text-2xl py-2 md:text-3xl dark:text-white">
+              Front End Developer
+            </h3>
             <p className="text-md py-5 leading-7 text-gray-800 md:text-xl max-w-3xl mx-auto dark:text-gray-200">
               Frontend developer by day, football fanatic by night (and also by
               day). Transforming ideas into interactive realities, with a
@@ -51,9 +56,15 @@ export default function Home() {
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 md:gap-24">
-            <AiFillTwitterCircle  className="hover:text-gray-400" href=""/>
-            <AiFillLinkedin className="hover:text-gray-400" href="https://www.linkedin.com/in/uzxyrr/"/>
-            <AiFillGithub  className="hover:text-gray-400" href="https://github.com/iamuzairbaba"/>
+            <AiFillTwitterCircle className="hover:text-gray-400" href="" />
+            <AiFillLinkedin
+              className="hover:text-gray-400"
+              href="https://www.linkedin.com/in/uzxyrr/"
+            />
+            <AiFillGithub
+              className="hover:text-gray-400"
+              href="https://github.com/iamuzairbaba"
+            />
           </div>
           <div className="relative mt-14 mx-auto h-80 w-80 md:h-96 md:w-96 mb-6">
             <Image src={ogo} layout="fill" objectFit="cover" />
@@ -80,7 +91,9 @@ export default function Home() {
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="dark:bg-gray-700 basis-1/3 flex-1 text-center shadow-lg p-10 rounded-xl flex flex-col justify-center items-center">
               <Image src={web1} className="w-[700px] lg:h-[400px]" />
-              <h3 className="text-xl font-medium pt-8 pb-2 dark:text-white">GYM BOI</h3>
+              <h3 className="text-xl font-medium pt-8 pb-2 dark:text-white">
+                GYM BOI
+              </h3>
               <p className="text-gray-800 dark:text-gray-200">
                 A responsive app made from GYM lover who can select their own
                 schedule for themselves.
@@ -104,7 +117,9 @@ export default function Home() {
             </div>
             <div className="dark:bg-gray-700 basis-1/3 flex-1 text-center shadow-lg p-10 rounded-xl flex flex-col justify-center items-center">
               <Image src={web5} className="w-[700px] lg:h-[400px]" />
-              <h3 className="text-xl font-medium pt-8 pb-2 dark:text-white">Weatherizz</h3>
+              <h3 className="text-xl font-medium pt-8 pb-2 dark:text-white">
+                Weatherizz
+              </h3>
               <p className="text-gray-800 dark:text-gray-200">
                 A responsive weather app made using ReactJS and TailwindCSS
                 using OpenWeatherAPI that shows the current weather and forecast
@@ -156,7 +171,9 @@ export default function Home() {
             </div>
             <div className="dark:bg-gray-700 basis-1/3 flex-1 text-center shadow-lg p-10 rounded-xl flex flex-col justify-center items-center">
               <Image src={web3} className="w-[700px] lg:h-[400px]" />
-              <h3 className="text-xl font-medium pt-8 pb-2 dark:text-white">Gehna Jewellery</h3>
+              <h3 className="text-xl font-medium pt-8 pb-2 dark:text-white">
+                Gehna Jewellery
+              </h3>
               <p className="text-gray-800 dark:text-gray-200">
                 A responsive landing page built using Figma design with ReactJS,
                 CSS for a Jewellery Shop.
@@ -180,7 +197,9 @@ export default function Home() {
             </div>
             <div className="dark:bg-gray-700 basis-1/3 flex-1 text-center shadow-lg p-10 rounded-xl flex flex-col justify-center items-center">
               <Image src={web4} className="w-[700px] lg:h-[400px]" />
-              <h3 className="text-xl font-medium pt-8 pb-2 dark:text-white">RIZZIPY</h3>
+              <h3 className="text-xl font-medium pt-8 pb-2 dark:text-white">
+                RIZZIPY
+              </h3>
               <p className="text-gray-800 dark:text-gray-200">
                 A responsive app made from food enthusiasts who love making
                 their own food. Made with ReactJS, Tailwind CSS and love.
@@ -203,6 +222,56 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </section>
+        <section>
+          <div className="text-center mt-10">
+            <h3 className="text-3xl py-1 dark:text-white ">My Skills</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a developer, I've worked and
+              learned many technologies. I bring a strong foundation in
+              programming languages, frameworks, and tools to deliver
+              high-quality solutions. My expertise includes:
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className=" flex flex-col items-center shadow-lg p-10 rounded-xl my-10  dark:bg-gray-700 dark:text-white flex-1">
+              <Image src={design} width={100} height={100} />
+              <h3 className="text-xl font-medium pt-8 pb-3  ">
+                Programming Languages
+              </h3>
+              <p className="text-gray-800 py-1 dark:text-white">JavaScript</p>
+              <p className="text-gray-800 py-1 dark:text-white">HTML & CSS</p>
+              <p className="text-gray-800 py-1 dark:text-white">C</p>
+              <p className="text-gray-800 py-1 dark:text-white">Python</p>
+            </div>
+            <div className=" flex flex-col items-center shadow-lg p-10 rounded-xl my-10  dark:bg-gray-700 dark:text-white flex-1">
+              <Image src={code} width={100} height={100} />
+              <h3 className="text-xl font-medium pt-8 pb-3 ">
+                FrameWorks and Libraries
+              </h3>
+              <p className="text-gray-800 py-1 dark:text-white">ReactJS</p>
+              <p className="text-gray-800 py-1 dark:text-white">Bootstrap</p>
+              <p className="text-gray-800 py-1 dark:text-white">NextJS</p>
+              <p className="text-gray-800 py-1 dark:text-white">Tailwind CSS</p>
+            </div>
+            <div className="shadow-lg p-10 rounded-xl my-10  dark:bg-gray-700 dark:text-white flex-1 flex flex-col items-center">
+              <Image src={consulting} width={100} height={100} />
+              <h3 className="text-xl font-medium pt-8 pb-3 ">Tools & Softwares</h3>
+              <p className="text-gray-800 py-1 dark:text-white">Git</p>
+              <p className="text-gray-800 py-1 dark:text-white">GitHub</p>
+              <p className="text-gray-800 py-1 dark:text-white">Figma</p>
+              <p className="text-gray-800 py-1 dark:text-white">VS Code</p>
+              <p className="text-gray-800 py-1 dark:text-white">MongoDB Compass</p>
+            </div>
+          </div>
+        </section>
+        <section>
+        <footer className="bg-white dark:bg-gray-800 py-8 text-center">
+      <p className="text-black dark:text-white text-sm">
+        &copy; 2024 Uzair Bashir. All rights reserved.
+      </p>
+    </footer>
+
         </section>
       </main>
     </div>
